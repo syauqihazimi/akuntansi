7afc7aff96cac506779cfe407aef3e17fecd55c8
Faaza Bil Amri <faazabilamri7@gmail.com>
2021-11-04 13:43:03 +0700

    update halaman login, perbaikan cetak pdf

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,9 +2,9 @@
 <footer class="footer">
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
-            <div class="copyright text-center text-xl-left text-muted">
+            <!-- <div class="copyright text-center text-xl-left text-muted">
               &copy; 2018 <a class="font-weight-bold ml-1" target="_blank">Hidayat Chandra</a>
-            </div>
+            </div> -->
           </div>
         </div>
       </footer>
