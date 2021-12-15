2a9d35f5eb21439847d36eeefcfbcf607fdf1bd2
Faaza Bil Amri <faazabilamri7@gmail.com>
2021-12-15 22:58:25 +0700

    tambahin about

--- application/views/user/about.php
+++ application/views/user/about.php
@@ -56,8 +56,65 @@
                 <div class="col">
                   <h3 class="mb-0">About</h3>
                   
+                  <img alt="Image placeholder" src="<?= base_url('assets\img\brand\blue.png') ?>">
+<p><strong>Autokuntansi</strong> adalah sistem informasi akuntansi yang membantu mencatat transaksi keuangan perusahaan
+    atau usaha Anda secara online melalui website.</p>
+<p>Dengan bantuan sistem ini, Anda tidak perlu membuat laporan keuangan secara manual, karena catatan transaksi yang
+    telah Anda input dalam sistem langsung dapat di cetak atau di download dalam format .pdf</p>
+<hr />
+<h2>Daftar Divisi</h2>
+<hr />
+<h4>Project Leader : Faaza Bil Amri</h4>
+<h3>Tim Analyst</h3>
+<ul>
+    <li>Muhamad Irsyad</li>
+    <li>Cut Aja Anis Layyinna</li>
+    <li>Afrida Chaerunnisa</li>
+    <li>Nanda Putri Styaningrum</li>
+</ul>
+<h3>Tim Desain</h3>
+<ul>
+    <li>Ari Lathifah</li>
+    <li>Nurul Islah</li>
+    <li>Ucha Aprilia</li>
+    <li>Ani Rosidah</li>
+    <li>Amanda Putri Rahayu</li>
+    <li>Dewi Syifa</li>
+</ul>
+<h3>Tim Programmer</h3>
+<ul>
+    <li>Faaza Bil Amri</li>
+    <li>Fahar Muharram</li>
+    <li>Insan Kamil</li>
+    <li>Nur Ihsan Fajar Ramadhan</li>
+    <li>Rijal Khatami</li>
+    <li>Muhammad Noor Rokhim</li>
+    <li>Muhammad Syauqi Hazimi</li>
+    <li>Jihan Fadhillah</li>
+</ul>
+<h3>Tim Dokumentasi</h3>
+<ul>
+    <li>Syfa Aula</li>
+    <li>Muhamad Rizky</li>
+</ul>
+<hr />
+<h2>Cara Penggunaan Aplikasi</h2>
+<hr />
+<h3>Cara Install</h3>
+<ul>
+    <li>Nyalakan XAMPP pada Laptop</li>
+    <li>Jalankan perintah pada prompt atau terminal composer install</li>
+    <li>Buatlah database siak, kemudian import database yang ada pada folder database</li>
+</ul>
+<hr />
+<h3>Akses Login</h3>
+<ul>
+    <li>Username: kelas5a</li>
+    <li>Username: finance</li>
+    <li>Username: accountant</li>
+    <li>Password: 12345678</li>
+</ul>
 
-                  okeylah
               <!-- ISI DISINI BROW!! -->
                 </div>
               </div>
